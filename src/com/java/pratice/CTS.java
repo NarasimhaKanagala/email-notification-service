1f6e66e21c90541eabad72058f3f08022463295f
kanag <kanagalanarasimha@gmail.com>
2023-12-20 23:09:29 +0530

    20/12/2023 programs

---
+++ src/com/java/pratice/CTS.java
@@ -0,0 +1,35 @@
+package com.java.pratice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.max;
+
+public class CTS {
+    public static void main(String[] args) {
+//
+//        Employee e1= new Employee(1,"abc",10000,"male");
+//        Employee e2= new Employee(2,"bbc",11000,"male");
+//        Employee e3= new Employee(3,"ccc",12000,"Female");
+//        Employee e4= new Employee(4,"ddc",13000,"Female");
+//        List<Employee> list = new ArrayList<>();
+//        list.add(e1);
+//        list.add(e2);
+//        list.add(e3);
+//        list.add(e4);
+//     Optional<Employee>   result = list.stream().
+//             filter(e -> "Female".equalsIgnoreCase(e.getGender()))
+//        .max(Comparator.comparingDouble(Employee::getSalary));
+//
+//       // result.ifPresent(System.out::println);
+//        System.out.println(result);
+// List<Integer> list =Arrays.asList(1,2,3,4,5);
+//  Optional<Integer> result=list.stream().max(Integer::compareTo);
+//  result.ifPresent(System.out::println);
+
+
+    }
+
+
+}
