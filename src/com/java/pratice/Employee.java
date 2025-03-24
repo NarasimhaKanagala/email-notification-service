d03a4c4dc41dc7760112ddd895feef77aefac3bc
Narasimha <kanagalanarasimha@gmail.com>
2025-03-24 18:01:30 +0530

    20/12/2023 programs

---
+++ src/com/java/pratice/Employee.java
@@ -0,0 +1,61 @@
+package com.java.pratice;
+
+public class Employee{
+
+    int id;
+    String name;
+
+    double salary;
+
+    String gender;
+
+    public Employee(int id, String name, double salary, String gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
